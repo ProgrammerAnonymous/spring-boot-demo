d6375afd58c2ffa60ec6c795430fabf25dfb3161
ChunChun Huang <cchuang0425@gmail.com>
2017-05-06 09:53:14 +0800

    update springboot basic example

--- spring-boot-basic/src/test/java/demo/springboot/SpringBootBasicApplicationTests.java
+++ spring-boot-basic/src/test/java/demo/springboot/SpringBootBasicApplicationTests.java
@@ -2,15 +2,22 @@ package demo.springboot;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootBasicApplicationTests {
 
+	@Autowired
+	private SpringBootBasicApplication application;
+
 	@Test
 	public void contextLoads() {
+		assertThat(application)
+				.isNotNull();
 	}
-
 }
