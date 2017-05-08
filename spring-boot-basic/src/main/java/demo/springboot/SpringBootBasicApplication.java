fa3cd876d5229f7199ca9ba4c3dea0d9899b5525
ChunChun Huang <cchuang0425@gmail.com>
2017-05-09 07:08:47 +0800

    closed #1 spring-boot-basic - basic project init

--- spring-boot-basic/src/main/java/demo/springboot/SpringBootBasicApplication.java
+++ spring-boot-basic/src/main/java/demo/springboot/SpringBootBasicApplication.java
@@ -1,12 +1,24 @@
 package demo.springboot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootBasicApplication {
+	private static final Logger log = LoggerFactory.getLogger(SpringBootBasicApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootBasicApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner sortRunner(){
+		return args -> {
+            log.info("display hello world in command line.");
+        };
 	}
 }
