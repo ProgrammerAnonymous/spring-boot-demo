f297e978d2809ab7417a1b533603be673feef540
ChunChun Huang <cchuang0425@gmail.com>
2017-05-12 06:55:20 +0800

    spring-boot-demo closed #3 spring-boot-basic application integration
    1. application integrated by CommandLineRunner

--- spring-boot-basic/src/main/java/demo/springboot/SpringBootBasicApplication.java
+++ spring-boot-basic/src/main/java/demo/springboot/SpringBootBasicApplication.java
@@ -1,9 +1,13 @@
 package demo.springboot;
 
+import java.util.Random;
+
 import demo.springboot.sort.SortService;
-import demo.springboot.sort.SortServiceImpl;
+import demo.springboot.sort.SortStats;
+import org.javatuples.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,15 +16,36 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class SpringBootBasicApplication {
     private static final Logger log = LoggerFactory.getLogger(SpringBootBasicApplication.class);
+    private static final int SIZE = 100000;
+    private static final int MAX_BOUND = 100000;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootBasicApplication.class, args);
     }
 
+    @Autowired
+    private SortService service;
+
     @Bean
     public CommandLineRunner sortRunner() {
         return args -> {
-            log.info("display hello world in command line.");
+            int[] instances = getRandomizedIntegers(SIZE);
+
+            Pair<SortStats, int[]> results = service.sort(instances);
+
+            log.info(results.getValue0().toString());
         };
+    }
+
+    private int[] getRandomizedIntegers(int size) {
+        int[] instances = new int[size];
+
+        Random rnd = new Random(System.currentTimeMillis());
+
+        for (int i = 0; i < instances.length; i++) {
+            instances[i] = rnd.nextInt(MAX_BOUND);
+        }
+
+        return instances;
     }
 }
